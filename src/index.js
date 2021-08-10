6c6d8b04b238ac511b5cd144d0b55be539c3c9ea
RAMANAND Ramanand <ram.anand@soprasteria.com>
2021-08-10 13:12:13 +0530

    Adding content to bookstore webapp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Bookstore from "./Bookstore";
+import "./index.css";
 //import FirstComponent from "./FirstComponent"; // herw we imported our component
 //import Details from "./Details";
-import NestedComponents from "./NestedComponents";
+//import NestedComponents from "./NestedComponents";
 /*here we used our component*/
 ReactDOM.render(
   <div>
-<NestedComponents />
- 
+<Bookstore />
   </div>, 
   document.getElementById('root')
 );
