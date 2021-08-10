4b4c9cf798790bc7af207613b087477d3f18c2a9
RAMANAND Ramanand <ram.anand@soprasteria.com>
2021-08-10 10:55:58 +0530

    Component Ordering

---
+++ src/Details.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Details() {
+    return (
+        <div>
+            <h1>Ramanand</h1>
+            <h2>Age = 21 years</h2>
+            </div>
+    )
+}
+
+export default Details;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FirstComponent from "./FirstComponent";
+import FirstComponent from "./FirstComponent"; // herw we imported our component
+import Details from "./Details";
 
+/*here we used our component*/
 ReactDOM.render(
-  <FirstComponent />,
+  <div>
+   <Details />
+  <FirstComponent />
+ 
+  </div>, 
   document.getElementById('root')
 );
 
