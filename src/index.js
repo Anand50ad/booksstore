b14b24f1c4003ceef00c68b756209055075616a8
RAMANAND Ramanand <ram.anand@soprasteria.com>
2021-08-10 11:44:21 +0530

    Nested Components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FirstComponent from "./FirstComponent"; // herw we imported our component
-import Details from "./Details";
-
+//import FirstComponent from "./FirstComponent"; // herw we imported our component
+//import Details from "./Details";
+import NestedComponents from "./NestedComponents";
 /*here we used our component*/
 ReactDOM.render(
   <div>
-   <Details />
-  <FirstComponent />
+<NestedComponents />
  
   </div>, 
   document.getElementById('root')
