7d3c42b3920e865cc404d25457cd7b241c5f921a
RAMANAND Ramanand <ram.anand@soprasteria.com>
2021-08-10 10:02:20 +0530

    My first component

---
+++ src/FirstComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FirstComponent() {
+    return (
+        <div>
+            <h2>This is my first component</h2>
+        </div>
+    )
+};
+
+export default FirstComponent;
